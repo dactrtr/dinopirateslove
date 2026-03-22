269c7f7c6f69e143210cb80e2820f75585590a15
Sebastian Zúñiga <dactrtr@gmail.com>
2026-03-22 11:25:05 +0900

    feat: add ResultsScreen entity with loading/win/lose states

---
+++ love2d-dancescene/src/entities/ResultsScreen.lua
@@ -0,0 +1,47 @@
+-- src/entities/ResultsScreen.lua
+-- Three states: "loading" (press A to start), "playing" (empty), "win", "lose"
+ResultsScreen = {}
+ResultsScreen.__index = ResultsScreen
+
+function ResultsScreen.new()
+    return setmetatable({ state = "loading" }, ResultsScreen)
+end
+
+function ResultsScreen:empty()
+    self.state = "playing"
+end
+
+function ResultsScreen:win()
+    if self.state ~= "win" then self.state = "win" end
+end
+
+function ResultsScreen:lose()
+    if self.state ~= "lose" then self.state = "lose" end
+end
+
+-- Called in update() when isDancing==false and condition==nil (i.e. pre-battle)
+function ResultsScreen:loadingScreen()
+    self.state = "loading"
+end
+
+function ResultsScreen:draw(scale)
+    scale = scale or 2
+    if self.state == "playing" then return end
+
+    -- Semi-transparent overlay
+    love.graphics.setColor(0, 0, 0, 0.7)
+    love.graphics.rectangle("fill", 0, 0, 800, 480)
+    love.graphics.setColor(1, 1, 1)
+
+    if self.state == "loading" then
+        love.graphics.printf("Press A to START BATTLE", 0, 200, 800, "center")
+    elseif self.state == "win" then
+        love.graphics.setColor(0.2, 1, 0.2)
+        love.graphics.printf("YOU WIN!\nPress A to continue", 0, 180, 800, "center")
+        love.graphics.setColor(1, 1, 1)
+    elseif self.state == "lose" then
+        love.graphics.setColor(1, 0.2, 0.2)
+        love.graphics.printf("YOU LOSE!\nPress A to continue", 0, 180, 800, "center")
+        love.graphics.setColor(1, 1, 1)
+    end
+end
