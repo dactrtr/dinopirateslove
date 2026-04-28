b41f8dc3b0a38cdb8adeac4276fe4f9e9c96bfa1
Sebastian Zúñiga <dactrtr@gmail.com>
2026-04-28 11:42:25 +0900

    feat: HitZone uses animated sprite (8-frame loop)

--- source/entities/UI/battle/HitZone.lua
+++ source/entities/UI/battle/HitZone.lua
@@ -1,17 +1,21 @@
 -- entities/UI/battle/HitZone.lua
+local anim8 = require 'libraries/anim8'
+
 HitZone = {}
 HitZone.__index = HitZone
 
 function HitZone.new(x, y, w, h)
-    return setmetatable({ x=x, y=y, w=w, h=h }, HitZone)
+    local self = setmetatable({ x=x, y=y, w=w, h=h }, HitZone)
+    self.image = love.graphics.newImage('assets/images/ui/battle/hitzone-table-10-40.png')
+    local g    = anim8.newGrid(w, h, self.image:getWidth(), self.image:getHeight())
+    self.anim  = anim8.newAnimation(g('1-8', 1), 0.06)
+    return self
 end
 
 local function aabbOverlap(ax, ay, aw, ah, bx, by, bw, bh)
     return ax < bx + bw and ax + aw > bx and
            ay < by + bh and ay + ah > by
 end
-
-function HitZone:update(dt) end
 
 function HitZone:overlapping(buttons)
     local result = {}
@@ -26,9 +30,12 @@ function HitZone:overlapping(buttons)
     return result
 end
 
+function HitZone:update(dt)
+    self.anim:update(dt)
+end
+
 function HitZone:draw(scale)
     scale = scale or 1
-    love.graphics.setColor(0.2, 0.8, 0.2, 0.5)
-    love.graphics.rectangle("fill", self.x * scale, self.y * scale, self.w * scale, self.h * scale)
     love.graphics.setColor(1, 1, 1)
+    self.anim:draw(self.image, self.x * scale, self.y * scale, 0, scale, scale)
 end
