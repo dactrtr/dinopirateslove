430207c76530cea7679b6519b57413caa1bce3ff
Sebastian Zúñiga <dactrtr@gmail.com>
2026-04-28 11:50:14 +0900

    feat: ResultsScreen uses real imagetable (ready/win/lose frames)

--- source/entities/UI/battle/ResultsScreen.lua
+++ source/entities/UI/battle/ResultsScreen.lua
@@ -2,35 +2,31 @@
 ResultsScreen = {}
 ResultsScreen.__index = ResultsScreen
 
+local FRAME = { playing=1, loading=2, win=3, lose=4 }
+
 function ResultsScreen.new()
-    return setmetatable({ state = "loading" }, ResultsScreen)
+    local self = setmetatable({ state = "loading" }, ResultsScreen)
+    self.image = love.graphics.newImage('assets/images/ui/battle/resultsdance-table-400-240.png')
+    local iw = self.image:getWidth()
+    local fw, fh = 400, 240
+    self.quads = {}
+    local nFrames = math.floor(iw / fw)
+    for i = 1, nFrames do
+        self.quads[i] = love.graphics.newQuad((i-1)*fw, 0, fw, fh, iw, fh)
+    end
+    return self
 end
 
-function ResultsScreen:empty()   self.state = "playing" end
-function ResultsScreen:win()     if self.state ~= "win"  then self.state = "win"  end end
-function ResultsScreen:lose()    if self.state ~= "lose" then self.state = "lose" end end
+function ResultsScreen:empty()         self.state = "playing" end
+function ResultsScreen:win()           if self.state ~= "win"  then self.state = "win"  end end
+function ResultsScreen:lose()          if self.state ~= "lose" then self.state = "lose" end end
 function ResultsScreen:loadingScreen() self.state = "loading" end
 
 function ResultsScreen:draw(scale)
     scale = scale or 1
-    if self.state == "playing" then return end
-
-    local W = 400 * scale
-    local H = 240 * scale
-
-    love.graphics.setColor(0, 0, 0, 0.7)
-    love.graphics.rectangle("fill", 0, 0, W, H)
+    local frameIdx = FRAME[self.state] or 1
+    local quad = self.quads[frameIdx]
+    if not quad then return end
     love.graphics.setColor(1, 1, 1)
-
-    if self.state == "loading" then
-        love.graphics.printf("Press SPACE to START BATTLE", 0, H * 0.42, W, "center")
-    elseif self.state == "win" then
-        love.graphics.setColor(0.2, 1, 0.2)
-        love.graphics.printf("YOU WIN!\nPress SPACE to continue", 0, H * 0.375, W, "center")
-        love.graphics.setColor(1, 1, 1)
-    elseif self.state == "lose" then
-        love.graphics.setColor(1, 0.2, 0.2)
-        love.graphics.printf("YOU LOSE!\nPress SPACE to continue", 0, H * 0.375, W, "center")
-        love.graphics.setColor(1, 1, 1)
-    end
+    love.graphics.draw(self.image, quad, 0, 0, 0, scale, scale)
 end
