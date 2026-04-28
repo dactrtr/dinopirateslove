2aa0ae35f50c08865eefa3933c556eaa647457b6
Sebastian Zúñiga <dactrtr@gmail.com>
2026-04-28 11:51:41 +0900

    feat: PlayerDance uses anim8 animated sprite (24-frame sheet)

--- source/entities/UI/battle/PlayerDance.lua
+++ source/entities/UI/battle/PlayerDance.lua
@@ -1,30 +1,52 @@
 -- entities/UI/battle/PlayerDance.lua
+local anim8 = require 'libraries/anim8'
+
 PlayerDance = {}
 PlayerDance.__index = PlayerDance
 
+local PLAYER_X = 0
+local PLAYER_Y = 26
+
 function PlayerDance.new(bpm)
-    return setmetatable({ bpm=bpm, state="idle" }, PlayerDance)
+    local self = setmetatable({ bpm = bpm }, PlayerDance)
+    self.image = love.graphics.newImage('assets/images/ui/battle/playerDance-table-246-214.png')
+    local g    = anim8.newGrid(246, 214, self.image:getWidth(), self.image:getHeight())
+    local fps  = 10
+    self.anims = {
+        idle   = anim8.newAnimation(g('1-4',  1), 1/fps),
+        jump   = anim8.newAnimation(g('5-9',  1), 1/fps),
+        crouch = anim8.newAnimation(g('11-15',1), 1/fps),
+        left   = anim8.newAnimation(g('16-20',1), 1/fps),
+        right  = anim8.newAnimation(g('21-24',1), 1/fps),
+    }
+    self.currentAnim = self.anims.idle
+    return self
 end
 
 function PlayerDance:changeAnimation(buttonKey)
-    -- Only responds to arrow buttons (directional), not A/B
-    if buttonKey == "leftButton" or buttonKey == "rightButton"
-    or buttonKey == "upButton"   or buttonKey == "downButton" then
-        self.state = buttonKey
+    local map = {
+        upButton    = "jump",
+        downButton  = "crouch",
+        leftButton  = "left",
+        rightButton = "right",
+    }
+    local name = map[buttonKey]
+    if name and self.anims[name] then
+        self.currentAnim = self.anims[name]
+        self.currentAnim:gotoFrame(1)
     end
 end
 
 function PlayerDance:setIdle()
-    self.state = "idle"
+    self.currentAnim = self.anims.idle
 end
 
-function PlayerDance:update(dt) end
+function PlayerDance:update(dt)
+    self.currentAnim:update(dt)
+end
 
 function PlayerDance:draw(scale)
     scale = scale or 1
-    local x, y = 60 * scale, 150 * scale
-    love.graphics.setColor(0.3, 0.6, 1)
-    love.graphics.rectangle("fill", x, y, 30 * scale, 40 * scale)
     love.graphics.setColor(1, 1, 1)
-    love.graphics.printf("P\n" .. self.state:sub(1,3), x, y + 4 * scale, 30 * scale, "center")
+    self.currentAnim:draw(self.image, PLAYER_X * scale, PLAYER_Y * scale, 0, scale, scale)
 end
